dc4fa5adb0ec2af8bf1bc0aa539295d78644ffc2
Robert Sadowksi <robertsadowski005@gmail.com>
2018-10-20 11:14:33 +0200

    Corrected and working exercise. Fill array with user stated amount of numbers of fibo numbers.

--- src/main/java/ArrayFibo.java
+++ src/main/java/ArrayFibo.java
@@ -6,25 +6,23 @@ public class ArrayFibo {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
 
-        int[] table = fillArraywithFibonacci(number);
-
+        long[] table = fillArraywithFibonacci(number);
+        print(table,number);
     }
 
-    private static int[] fillArraywithFibonacci(int n){
-        int[] table = new int[n];
-        table[0]=1;
-        table[1]=1;
-        for (int i = 2; i <n ; i++){
-            table[i]= table[i-1] + table[i-2];
+    private static long[] fillArraywithFibonacci(int n) {
+        long[] table = new long[n];
+        table[0] = 1;
+        table[1] = 1;
+        for (int i = 2; i < n; i++) {
+            table[i] = table[i - 1] + table[i - 2];
         }
         return table;
     }
 
-
-
-
-    private static long fibonacciNumberResault(long number) {
-        if (number <= 1) return number;
-        else return fibonacciNumberResault(number - 1) + fibonacciNumberResault(number - 2);
+    private static void print(long[] table, int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print(table[i] + ", ");
+        }
     }
 }
